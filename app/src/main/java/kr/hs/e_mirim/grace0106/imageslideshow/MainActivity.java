4107fcbcca6387f18ffdd3c7b608926eaf09db65
HyeonJu Jeong <0106grace@naver.com>
2016-05-26 20:53:52 +0900

    #3 set Time

--- app/src/main/java/kr/hs/e_mirim/grace0106/imageslideshow/MainActivity.java
+++ app/src/main/java/kr/hs/e_mirim/grace0106/imageslideshow/MainActivity.java
@@ -1,13 +1,41 @@
 package kr.hs.e_mirim.grace0106.imageslideshow;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ViewFlipper;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    ViewFlipper flip;
+    EditText editTime;
+    int time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        flip = (ViewFlipper)findViewById(R.id.view_flip);
+        editTime=(EditText)findViewById(R.id.edit_time);
+        Button butStart = (Button)findViewById(R.id.but_start);
+        butStart.setOnClickListener(this);
+        Button butStop = (Button)findViewById(R.id.but_stop);
+        butStop.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.but_start:
+                time=(int)(Double.parseDouble(editTime.getText().toString())*1000);
+                flip.setFlipInterval(time);
+                flip.startFlipping();
+                break;
+            case R.id.but_stop:
+                flip.stopFlipping();
+                break;
+        }
     }
 }
